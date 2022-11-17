9e60360f4b36bdf6a7e8861c27391bbaee8b21c7
Jonitha42 <yonipardo4@gmail.com>
2022-11-17 12:34:07 -0300

    Deploy

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/*.html"
+  ],
+  theme: {
+    screens: {
+      sm: "360px",
+      md: "768px",
+      lg: "1440px",
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    extend: {
+      fontFamily: {
+        raleway: "'Raleway', 'sans-serif'",
+      },
+    },
+  },
+  plugins: [],
+}
